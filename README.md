2d099e7a81c62a8828c04be043cbca24b1b96cfc
Zamoroka <zapashok0@gmail.com>
2017-09-11 14:05:16 +0300

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+developed on enviornment:
+ - PHP 7.0.17-1
+ - NGINX 1.11.10
+ - MYSQL 5.6.28-76.1
+ - Magento 2.1.8
---
+++ app/code/Zamoroka/PayPalAllCurrencies/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Zamoroka_PayPalAllCurrencies',
+    __DIR__
+);

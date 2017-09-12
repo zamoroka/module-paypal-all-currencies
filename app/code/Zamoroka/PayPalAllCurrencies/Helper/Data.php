855bebdafd0e19dbdd61d09bdce6cf27d9de35c0
Zamoroka <zapashok0@gmail.com>
2017-09-12 14:17:13 +0300

    admin config

---
+++ app/code/Zamoroka/PayPalAllCurrencies/Helper/Data.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Zamoroka\PayPalAllCurrencies\Helper;
+
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Framework\ObjectManagerInterface;
+use Magento\Framework\App\Helper\Context;
+use Magento\Store\Model\ScopeInterface;
+
+/**
+ * Class Data
+ *
+ * @package Zamoroka\PayPalAllCurrencies\Helper
+ */
+class Data extends AbstractHelper
+{
+    protected $storeManager;
+
+    protected $objectManager;
+
+    const XML_PATH_GENERAL = 'zamoroka_paypalallcurrencies/general/';
+
+    /**
+     * Data constructor.
+     *
+     * @param \Magento\Framework\App\Helper\Context      $context
+     * @param \Magento\Framework\ObjectManagerInterface  $objectManager
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     */
+    public function __construct(
+        Context $context,
+        ObjectManagerInterface $objectManager,
+        StoreManagerInterface $storeManager
+    ) {
+        $this->objectManager = $objectManager;
+        $this->storeManager = $storeManager;
+        parent::__construct($context);
+    }
+
+    /**
+     * @param string   $field
+     * @param null|int $storeId
+     * @return mixed
+     */
+    public function getConfigValue($field, $storeId = null)
+    {
+        return $this->scopeConfig->getValue(
+            $field, ScopeInterface::SCOPE_STORE, $storeId
+        );
+    }
+
+    /**
+     * @param string   $code
+     * @param null|int $storeId
+     * @return mixed
+     */
+    public function getGeneralConfig($code, $storeId = null)
+    {
+        return $this->getConfigValue(self::XML_PATH_GENERAL . $code, $storeId);
+    }
+
+    /**
+     * @return bool
+     */
+    public function isModuleEnabled()
+    {
+        if (parent::isModuleOutputEnabled('Zamoroka_PayPalAllCurrencies')
+            && $this->getGeneralConfig('enabled')
+        ) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPayPalCurrency()
+    {
+        return $this->getGeneralConfig('paypalcurrency');
+    }
+}
---
+++ app/code/Zamoroka/PayPalAllCurrencies/Model/Config/PayPalCurrencies.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Zamoroka\PayPalAllCurrencies\Model\Config;
+
+/**
+ * Class PayPalCurrencies
+ *
+ * @package Zamoroka\PayPalAllCurrencies\Model\Config
+ */
+class PayPalCurrencies implements \Magento\Framework\Option\ArrayInterface
+{
+    /**
+     * Currency codes supported by PayPal methods
+     *
+     * @var string[]
+     */
+    protected $_supportedCurrencyCodes
+        = [
+            'AUD',
+            'CAD',
+            'CZK',
+            'DKK',
+            'EUR',
+            'HKD',
+            'HUF',
+            'ILS',
+            'JPY',
+            'MXN',
+            'NOK',
+            'NZD',
+            'PLN',
+            'GBP',
+            'RUB',
+            'SGD',
+            'SEK',
+            'CHF',
+            'TWD',
+            'THB',
+            'USD',
+        ];
+
+    /**
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        $optionArray = [];
+        foreach ($this->_supportedCurrencyCodes as $currencyCode) {
+            $optionArray[] = ['value' => $currencyCode, 'label' => $currencyCode];
+        }
+
+        return $optionArray;
+    }
+}
